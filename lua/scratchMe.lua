20894633056d058bac781f737de1ceb30cefe93d
DrinoSan <s.becirbegovic@gmail.com>
2024-08-09 23:43:55 +0200

    Small stuff

--- lua/scratchMe.lua
+++ lua/scratchMe.lua
@@ -3,21 +3,19 @@ local augroup = vim.api.nvim_create_augroup("ScratchMe", { clear = true })
 local function createBuffer()
 	local buf = vim.api.nvim_create_buf(true, true)
 	vim.api.nvim_buf_set_name(buf, "*ScratchMe*")
-	vim.api.nvim_set_option_value("filetype", "lua", { buf = buf })
+	vim.api.nvim_set_option_value("filetype", "txt", { buf = buf })
 
 	return buf
 end
 
 local function main()
-	print("ScratchMe Loaded Hi")
-
 	local buf = createBuffer()
 
 	vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "-- Welcome to Neovim!", "" })
 
 	--	vim.api.nvim_win_set_buf(0, buf)
 
-	vim.api.nvim_win_set_cursor(0, { vim.api.nvim_buf_line_count(buf), 0 })
+	vim.apinvim_win_set_cursor(0, { vim.api.nvim_buf_line_count(buf), 0 })
 end
 
 local function setup()
